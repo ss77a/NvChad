be8622b7f3989f99783192bf95b9e5fe50b7dd3d
Sam <ss7i@outlook.com>
2022-10-14 20:40:31 +0100

    themes working lualine working - like a charm

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -173,8 +173,6 @@ local plugins = {
   ["kyazdani42/nvim-tree.lua"] = {
     ft = "alpha",
     cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-  
-
     config = function()
       require "plugins.configs.nvimtree"
     end,
@@ -206,23 +204,60 @@ local plugins = {
     end,
   },
 
-  [ "themercorp/themer.lua"] = {
+  ["themercorp/themer.lua"] = {
     disabled = false,
---    module = "themer",
     config = function()
-	    require("themer").setup({
-	      colorscheme = "rose_pine",
-	      styles = {
-	        ["function"] = { style = 'italic' },
-	        functionbuiltin = { style = 'italic' },
-	        variable = { style = 'italic' },
-	        variableBuiltIn = { style = 'italic' },
-	        parameter  = { style = 'italic' },
-	      },
-	    })
+      require "plugins.configs.themer"
+    end
+  },
+
+  --[[lualine]]
+  ["nvim-lualine/lualine.nvim"] = {
+    disabled = false,
+    config = function()
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          component_separators = { left = '', right = ''},
+          section_separators = { left = '', right = ''},
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+          }
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {'filename'},
+          lualine_x = {'location'},
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {}
+      }
     end
   }
-
+  --]]
 }
 
 -- Load all plugins
