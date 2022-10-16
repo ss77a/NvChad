123a2ca5cf0f1c90ed36da5a3cd11187cbcd35a7
Sam <ss7i@outlook.com>
2022-10-16 14:47:06 +0100

    move configs to config/ dir for cleaner plugin init (now I understand syntax variation a bit more in lua and have helpful intergrations working); add cinnamon smooth scroll plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -204,6 +204,8 @@ local plugins = {
     end,
   },
 
+  -- added plugins
+
   ["themercorp/themer.lua"] = {
     disabled = false,
     config = function()
@@ -214,46 +216,7 @@ local plugins = {
   ["nvim-lualine/lualine.nvim"] = {
     disabled = false,
     config = function()
-      require('lualine').setup {
-        options = {
-          icons_enabled = true,
-          theme = 'auto',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-          ignore_focus = {},
-          always_divide_middle = true,
-          globalstatus = false,
-          refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-          }
-        },
-        sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = {'filename'},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
-          lualine_y = {},
-          lualine_z = {}
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-      }
+     require "plugins.configs.lualine"
     end
   },
 
@@ -302,83 +265,14 @@ local plugins = {
   ["Pocco81/TrueZen.nvim"] = {
     disabled = false,
     config = function ()
-      require("true-zen").setup {
-        window = {
-          backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-            -- height and width can be:
-            -- * an absolute number of cells when > 1
-            -- * a percentage of the width / height of the editor when <= 1
-            -- * a function that returns the width or the height
-            width = 120, -- width of the Zen window
-            height = 1, -- height of the Zen window
-            -- by default, no options are changed for the Zen window
-            -- uncomment any of the options below, or add other vim.wo options you want to apply
-            options = {
-              -- signcolumn = "no", -- disable signcolumn
-              -- number = false, -- disable number column
-              -- relativenumber = false, -- disable relative numbers
-              -- cursorline = false, -- disable cursorline
-              -- cursorcolumn = false, -- disable cursor column
-              -- foldcolumn = "0", -- disable fold column
-              -- list = false, -- disable whitespace characters
-            },
-          },
-          plugins = {
-            -- disable some global vim options (vim.o...)
-            -- comment the lines to not apply the options
-            options = {
-              enabled = true,
-              ruler = false, -- disables the ruler text in the cmd line area
-              showcmd = false, -- disables the command in the last line of the screen
-            },
-            twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
-            gitsigns = { enabled = false }, -- disables git signs
-            tmux = { enabled = false }, -- disables the tmux statusline
-            -- this will change the font size on kitty when in zen mode
-            -- to make this work, you need to set the following kitty options:
-            -- - allow_remote_control socket-only
-            -- - listen_on unix:/tmp/kitty
-            kitty = {
-              enabled = false,
-              font = "+4", -- font size increment
-            },
-          },
-          -- callback where you can add custom code when the Zen window opens
-          on_open = function(win)
-          end,
-          -- callback where you can add custom code when the Zen window closes
-          on_close = function()
-          end,
-        }
+      require "plugins.configs.true-zen"
     end
   },
 
   ["folke/twilight.nvim"] = {
     disabled = false,
     config = function ()
-      require("twilight").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-        dimming = {
-          alpha = 0.25, -- amount of dimming
-          -- we try to get the foreground from the highlight groups or fallback color
-          color = { "Normal", "#ffffff" },
-          term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
-          inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
-        },
-        context = 10, -- amount of lines we will try to show around the current line
-        treesitter = true, -- use treesitter when available for the filetype
-        -- treesitter is used to automatically expand the visible text,
-        -- but you can further control the types of nodes that should always be fully expanded
-        expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
-          "function",
-          "method",
-          "table",
-          "if_statement",
-        },
-        exclude = {},
-      }
+      require "plugins.configs.twilight"
     end
   },
 
@@ -387,6 +281,13 @@ local plugins = {
       config = function()
          require "plugins.configs.null-ls"
       end,
+  },
+
+  ["declancm/cinnamon.nvim"] = {
+    disabled = false,
+    config = function ()
+      require "plugins.configs.cinnamon"
+    end
   }
 }
 
